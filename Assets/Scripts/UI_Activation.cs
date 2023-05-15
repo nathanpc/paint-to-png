df9676459446484c73c9467972f3241ae90e0326
DiogoSilv4 <73476561+DiogoSilv4@users.noreply.github.com>
2023-05-15 18:09:16 +0100

    ImprovedCanUI

--- Assets/Scripts/UI_Activation.cs
+++ Assets/Scripts/UI_Activation.cs
@@ -6,12 +6,16 @@ public class UI_Activation : MonoBehaviour
 {
     public bool activateUI = false;
 
+    [SerializeField] private GameObject UI_Component;
     [SerializeField] private GameObject cap;
     [SerializeField] private GameObject capRegulator;
     [SerializeField] private GameObject colorPicker;
     [SerializeField] private GameObject AIModeButton;
     [SerializeField] private GameObject Lip;
     [SerializeField] private Color CanColor;
+
+    [SerializeField] private Color[] Colors = {Color.black, Color.white ,Color.grey,  Color.blue, Color.cyan, Color.green, Color.yellow, Color.magenta};
+    private int colorCount = 0;
 
     private float regulatorScale = 1f;
 
@@ -20,9 +24,7 @@ public class UI_Activation : MonoBehaviour
     void Start()
     {
         activateUI = false;
-        capRegulator.SetActive(false);
-        colorPicker.SetActive(false);
-        AIModeButton.SetActive(false);
+        UI_Component.SetActive(false);
         cap.GetComponent<Outline>().enabled = false;
     }
 
@@ -33,17 +35,50 @@ public class UI_Activation : MonoBehaviour
         {
             startActivation();
         }
+        else
+        {
+            deactivateUI();
+        }
     }
 
     private void startActivation()
     {
-        capRegulator.SetActive(true);
-        colorPicker.SetActive(true);
-        AIModeButton.SetActive(true);
+        UI_Component.SetActive(true);
+
+        UI_Component.GetComponent<Animator>().SetBool("Active", true);
+
+    }
+    private void deactivateUI()
+    {
+        UI_Component.GetComponent<Animator>().SetBool("Active", false);
+       
     }
     private void activateAIMode()
     {
         cap.GetComponent<Outline>().enabled = true;
     }
+    private void SelectColorLeft()
+    {
+        colorCount -= 1;
+        countWithinBounds(colorCount);
+    }
+    private void SelectColorRight()
+    {
 
+    }
+    private void countWithinBounds(int count)
+    {
+        if (count >= Colors.Length)
+        {
+            count = 0;
+        }
+        else if (count < 0)
+        {
+            count = Colors.Length - 1;
+        }
+    }
+    private void updateCanColor()
+    {
+        Lip.GetComponent<Material>().color = Colors[colorCount];
+    }
 }
